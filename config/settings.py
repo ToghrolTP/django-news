c2c667383a21732a237892afd9620c548585b84c
Mohammadreza Malekmohammadi <toghroltheprogrammer@gmail.com>
2023-07-04 17:12:19 +0330

    Update some bootstrap elements; Add crispy_forms.

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_bootstrap4',
     "accounts",
-    "pages"
+    "pages",
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 AUTH_USER_MODEL = 'accounts.CustomUser' # new
 
