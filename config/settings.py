290b46c4eba8db05c4b8bcbb93f7491701c92274
Mohammadreza Malekmohammadi <toghroltheprogrammer@gmail.com>
2023-07-05 11:17:17 +0330

    Add .

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Login/Logout directions
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
