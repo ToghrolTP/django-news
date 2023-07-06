235a4905c2cff468656cc6c16015d4c9908bf77e
Mohammadreza Malekmohammadi <toghroltheprogrammer@gmail.com>
2023-07-06 17:55:37 +0330

    Add delete functionality for each post.

--- pages/urls.py
+++ pages/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (HomeListView,
                     PostDetailView,
                     PostCreateView,
-                    PostUpdateView)
+                    PostUpdateView,
+                    PostDeleteView,)
 
 urlpatterns = [
     path("", HomeListView.as_view(), name="home"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("post/new/", PostCreateView.as_view(), name = "post_new"),
-    path("post/<int:pk>/update", PostUpdateView.as_view(), name = "post_update")
+    path("post/<int:pk>/update", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post_delete"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import (ListView,
                                   DetailView,)
 from django.views.generic.edit import (CreateView,
-                                       UpdateView)
+                                       UpdateView,
+                                       DeleteView,)
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -26,3 +28,9 @@ class PostUpdateView(UpdateView):
     model = Post
     template_name = "post_update.html"
     fields = ["title", "body"]
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
